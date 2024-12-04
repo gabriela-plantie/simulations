7cd93fe042bb5d05a98e6c4d9a20dca4645083dc
gaby <5839522+gabriela-plantie@users.noreply.github.com>
2024-12-04 19:38:42 +0100

    eliminate randomness in tests

--- tests/test_shifts_creation.py
+++ tests/test_shifts_creation.py
@@ -5,4 +5,6 @@ def test_rider_demand_satisfaction():
     estimated_rider_demand = {0: 10, 1: 9, 2: 8}
     cp_shift = CPShifts(estimated_rider_demand)
     shifts = cp_shift.create_shifts(min_len=3, max_len=3)
-    assert shifts == {(0, 3): 9, (3, 0): 1}  # 9 shifts of len 3 and startime 0
+    assert {s: v for s, v in shifts.items() if s[1] > 0} == {
+        (0, 3): 9
+    }  # 9 shifts of len 3 and startime 0
