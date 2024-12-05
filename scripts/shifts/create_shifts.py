8c748fe01fedc7050dd26f882d0ab503c72018ba
gaby <5839522+gabriela-plantie@users.noreply.github.com>
2024-12-05 09:59:42 +0100

    wip refactor and add test cases - still failing
    wip refactor will failing tests
    
    wip refactor with failing tests
    
    wip refactor with failing tests
    
    wip- improve logic
    
    wip refactor with failing tests

--- scripts/shifts/create_shifts.py
+++ scripts/shifts/create_shifts.py
@@ -31,7 +31,6 @@ class CPShifts:
 
     def create_shifts(self, max_len, min_len):
         max_shifts = self.get_shifts_len_possibilities(max_len=max_len, min_len=min_len)
-        model = cp_model.CpModel()
         times = self.rider_demand.keys()
 
         print(f"Times: {list(times)}")
@@ -40,72 +39,129 @@ class CPShifts:
 
         max_total_shifts = int(max_shifts[min_len])
 
-        t_shifts = [
-            model.NewIntVar(
-                lb=min(times),
-                ub=max(max(times) - min_len + 1, 0),
-                name=f"t_for_shift_id_{s}",
-            )
-            for s in range(max_total_shifts)
-        ]
+        model = cp_model.CpModel()
 
-        len_domain = [0] + list(range(min_len, max_len + 1))
-        print(f"len_domain {len_domain}")
-        len_shifts = [
-            model.NewIntVarFromDomain(
-                domain=cp_model.Domain.FromValues(len_domain),
-                name=f"len_for_shift_id_{s}",
-            )
-            for s in range(max_total_shifts)
-        ]
+        # Arrays with one int per shift with the start time and length of the shift
+        t_shifts, len_shifts = self.create_shift_variables(
+            model=model,
+            max_len=max_len,
+            min_len=min_len,
+            times=times,
+            max_total_shifts=max_total_shifts,
+        )
 
-        active_riders_in_t = []
-        for t in times:
-            # Boolean variables for active shifts at time t
-            active_shifts_at_t = [
-                model.NewBoolVar(name=f"is_active_shift_{s}_at_{t}")
-                for s in range(max_total_shifts)
-            ]
+        # Array with one bool per shift per time stating if the shift was active
+        active_shift_at_t = self.create_shift_active_in_each_t(
+            model=model,
+            times=times,
+            max_total_shifts=max_total_shifts,
+            t_shifts=t_shifts,
+            len_shifts=len_shifts,
+        )
 
-            # Add constraints to link activity to start time and length
-            for s, (t_ini, _len) in enumerate(zip(t_shifts, len_shifts)):
-                model.Add(t_ini <= t).OnlyEnforceIf(active_shifts_at_t[s])
-                model.Add(t < t_ini + _len).OnlyEnforceIf(active_shifts_at_t[s])
+        # Array with one bool per shift stating if the shift was ever active
+        ever_active_shift = self.create_ever_active_shift(
+            model=model,
+            times=times,
+            max_total_shifts=max_total_shifts,
+            active_shift_at_t=active_shift_at_t,
+        )
 
-                # Create Boolean variables for the conditions
-                shift_started_before_t = model.NewBoolVar(
-                    f"shift_{s}_started_before_{t}"
-                )
-                shift_ended_after_t = model.NewBoolVar(f"shift_{s}_ended_after_{t}")
+        # TODO una q diga q solo puede estar activo una vez
+        # TODO q saque las simetrias
+        # (para todos los shifts de igual longitud ordenar lexicograficamente)
+        # TODO en casos de equivalencias
+        # (2 shifts de 1 vs 1 shift de dos, tomar un criterio)
+        # (esto se llama dominancia tambien?)
 
-                # Link constraints to Boolean variables
-                # shift_started_before_t -> t_ini <= t  A -> B1, A-> B2
-                model.Add(t_ini <= t).OnlyEnforceIf(shift_started_before_t)
-                model.Add(t < t_ini + _len).OnlyEnforceIf(shift_ended_after_t)
+        # for debuggin purposes
+        # model.Add(len_shifts[0] <= 3)
+        # model.Add(len_shifts[0] >= 3)
+        # model.Add(t_shifts[0] <= 0)
+        # model.Add(t_shifts[0] >= 0)
+        # model.Add(len_shifts[1] <= 2)
+        # model.Add(len_shifts[1] >= 2)
+        # model.Add(t_shifts[1] <= 0)
+        # model.Add(t_shifts[1] >= 0)
 
-                # (B1/\B2) -> A
-                model.AddBoolAnd(
-                    [shift_started_before_t, shift_ended_after_t]
-                ).OnlyEnforceIf(active_shifts_at_t[s])
+        self.place_non_actives_shifts(
+            model=model,
+            times=times,
+            max_total_shifts=max_total_shifts,
+            t_shifts=t_shifts,
+            len_shifts=len_shifts,
+            ever_active_shift=ever_active_shift,
+            min_len=min_len,
+        )
 
-            active_riders_in_t.append(sum(active_shifts_at_t))
+        # enforce inside grid for active shifts!!!
+        self.constraint_ever_active_shifts_inside_grid_time(
+            model=model,
+            times=times,
+            t_shifts=t_shifts,
+            len_shifts=len_shifts,
+            max_total_shifts=max_total_shifts,
+            ever_active_shift=ever_active_shift,
+        )
 
-            for s in range(max_total_shifts):
-                model.Add(len_shifts[s] == 0).OnlyEnforceIf(active_shifts_at_t[s].Not())
+        active_riders_in_t = self.create_active_riders_in_t(
+            model=model,
+            times=times,
+            max_total_shifts=max_total_shifts,
+            active_shift_at_t=active_shift_at_t,
+        )
 
         # define slack var for each t
-        slacks = [
-            model.NewIntVar(
-                lb=-self.rider_demand[t],
-                ub=+self.rider_demand[t],
-                name=f"slack_for_time_{t}",
-            )
-            for t in times
-        ]
-        for t in times:
-            model.Add(active_riders_in_t[t] + slacks[t] == self.rider_demand[t])
+        slacks = self.create_slacks_for_demand_in_t(
+            model=model, times=times, active_riders_in_t=active_riders_in_t
+        )
 
         # creo el abs de la variable slack
+        abs_slacks = self.create_abs_slacks_for_demand_in_t(
+            times=times, model=model, slacks=slacks
+        )
+
+        objective = model.NewIntVar(
+            lb=0,  # Valor absoluto siempre positivo
+            ub=max(self.rider_demand) * len(times),
+            name="objective",
+        )
+        model.Add(objective == sum(abs_slacks))
+        model.Minimize(objective)
+
+        solver = cp_model.CpSolver()
+        status = solver.Solve(model)
+        print(f"objective reached: {solver.objective_value}")
+        print(solver.StatusName(status))
+        print(f"active riders in t :{[solver.value(r) for r in active_riders_in_t]}")
+        sol = self.format_output(
+            solver=solver, t_shifts=t_shifts, len_shifts=len_shifts
+        )
+        return solver.objective_value, Counter(sol)
+
+    def constraint_ever_active_shifts_inside_grid_time(
+        self, model, times, max_total_shifts, t_shifts, len_shifts, ever_active_shift
+    ):
+        for s in range(max_total_shifts):
+            model.Add(t_shifts[s] >= min(times)).OnlyEnforceIf(ever_active_shift[s])
+            model.Add(t_shifts[s] + len_shifts[s] - 1 <= max(times)).OnlyEnforceIf(
+                ever_active_shift[s]
+            )
+            model.Add(t_shifts[s] == max(times) + 1).OnlyEnforceIf(
+                ever_active_shift[s].Not()
+            )
+
+    def format_output(self, t_shifts, len_shifts, solver):
+        sol = list(
+            zip(
+                [solver.Value(s) for s in t_shifts],
+                [solver.Value(s) for s in len_shifts],
+            )
+        )
+        print(sol)
+        return sol
+
+    def create_abs_slacks_for_demand_in_t(self, times, model, slacks):
         abs_slacks = [
             model.NewIntVar(
                 lb=0,  # Valor absoluto siempre positivo
@@ -117,51 +173,146 @@ class CPShifts:
         for t in times:
             model.Add(abs_slacks[t] >= slacks[t])
             model.Add(abs_slacks[t] >= -slacks[t])
+        return abs_slacks
 
-        objective = sum(abs_slacks)
-        model.Minimize(objective)
+    def create_slacks_for_demand_in_t(self, times, model, active_riders_in_t):
+        slacks = [
+            model.NewIntVar(
+                lb=-self.rider_demand[t],
+                ub=+self.rider_demand[t],
+                name=f"slack_for_time_{t}",
+            )
+            for t in times
+        ]
+        for t in times:
+            model.Add(active_riders_in_t[t] + slacks[t] == self.rider_demand[t])
+        return slacks
 
-        solver = cp_model.CpSolver()
-        status = solver.Solve(model)
-        print(solver.StatusName(status))
-        sol = list(
-            zip(
-                [solver.Value(s) for s in t_shifts],
-                [solver.Value(s) for s in len_shifts],
+    def create_active_riders_in_t(
+        self, model, times, max_total_shifts, active_shift_at_t
+    ):
+        active_riders_in_t = [
+            model.NewIntVar(
+                lb=0,
+                ub=self.rider_demand[t] * 2,  # improev this ub
+                name=f"active_riders_in_{t}",
+            )
+            for t in times
+        ]
+        for t in times:
+            model.Add(
+                active_riders_in_t[t]
+                == sum(active_shift_at_t[t][s] for s in range(max_total_shifts))
+            )
+
+        return active_riders_in_t
+
+    def place_non_actives_shifts(
+        self,
+        times,
+        max_total_shifts,
+        model,
+        t_shifts,
+        len_shifts,
+        ever_active_shift,
+        min_len,
+    ):
+        """
+        If the shift is not active then the start time and the length must be 0
+        """
+        for s in range(max_total_shifts):
+            model.Add(len_shifts[s] == 0).OnlyEnforceIf(ever_active_shift[s].Not())
+
+            model.Add(len_shifts[s] > 0).OnlyEnforceIf(ever_active_shift[s])
+
+            model.Add(t_shifts[s] == max(times) + 1).OnlyEnforceIf(
+                ever_active_shift[s].Not()
+            )
+
+            model.Add(t_shifts[s] <= max(times) - (min_len - 1)).OnlyEnforceIf(
+                ever_active_shift[s]
+            )
+
+    # def create_times_acive_shift()
+
+    def create_ever_active_shift(
+        self, times, max_total_shifts, model, active_shift_at_t
+    ):
+        ever_active_shift = [
+            model.NewBoolVar(name=f"ever_active_shift_{s}")
+            for s in range(max_total_shifts)
+        ]
+
+        for s in range(max_total_shifts):
+            model.AddBoolOr([active_shift_at_t[t][s] for t in times]).OnlyEnforceIf(
+                ever_active_shift[s]
+            )
+
+            model.Add(sum([active_shift_at_t[t][s] for t in times]) == 0).OnlyEnforceIf(
+                ever_active_shift[s].Not()
+            )
+
+        return ever_active_shift
+
+    def create_shift_variables(self, max_len, min_len, times, max_total_shifts, model):
+        """
+        Arrays with one int per shift with the start time and length of the shift.
+        If lenght is 1 then the shift is only in one time slot.
+        """
+        time_out_of_bounds = max(times) + 1  # for shifts not used
+        t_domain = list(
+            set(
+                [time_out_of_bounds]
+                + list(range(min(times), max(max(times) - min_len + 2, 0)))
             )
         )
-        print(f"objective reached: {solver.objective_value}")
-        return Counter(sol)
+        t_domain.sort()
+        print(f"t_domain {t_domain}")
+        t_shifts = [
+            model.NewIntVarFromDomain(
+                domain=cp_model.Domain.FromValues(t_domain),
+                name=f"t_for_shift_id_{s}",
+            )
+            for s in range(max_total_shifts)
+        ]
 
-        # # {long 3: [3,4,8]} # a que hora empieza cada uno
-        # start_time_of_shifts_by_type = {}
-        # for _len, q in max_shifts.items():
-        #     start_time_of_shifts_by_type[_len] = {}
-        #     for i in range(q):
-        #         start_time_of_shifts_by_type[_len][i] = model.new_int_var(
-        #             lb=min(times),
-        #             ub=max(times) + 1, # the last one is outside
-        #             name=f"t_for_shift-id_{i}_with_len_{_len}")
+        len_domain = list(set([0] + list(range(min_len, max_len + 1))))
+        len_domain.sort()
+        print(f"len_domain {len_domain}")
+        len_shifts = [
+            model.NewIntVarFromDomain(
+                domain=cp_model.Domain.FromValues(len_domain),
+                name=f"len_for_shift_id_{s}",
+            )
+            for s in range(max_total_shifts)
+        ]
 
-        # shifts_by_time = {}
-        # for t in range(times):
-        #     shifts_by_time[t] = {}
-        #     for _len in range(min_len, max_len+1):
-        #         model.new_int_var(
-        #             lb=0,
-        #             ub=max_shifts[_len],
-        #             name=f"num_shifts_in_t_{t}_with_len_{_len}")
+        return t_shifts, len_shifts
 
-    # minizinc
-    # constraint cumulative(x, size, size, height);
-    # constraint cumulative(y, size, size, width);
+    def create_shift_active_in_each_t(
+        self, times, max_total_shifts, model, t_shifts, len_shifts
+    ):
+        """
+        Array with one bool per shift per time stating if the shift was active.
+        """
+        active_shift_at_t = {}
 
-    # ortools
-    # Adds Cumulative(intervals, demands, capacity).
-    # for all t: sum(demands[i] if (start(intervals[i]) <= t < end(intervals[i]))
-    # and (intervals[i] is present)) <= capacity
-    # capacity = demanda en t, pero no veo capacity en t
+        for t in times:
+            active_shift_at_t[t] = [
+                model.NewBoolVar(name=f"is_active_shift_{s}_at_{t}")
+                for s in range(max_total_shifts)
+            ]
 
-    # x[s1] + size[s1] <= x[s2] \/  # s2 esta a la derecha
-    # x[s2] + size[s2] <= x[s1] \/  # o S1 esta a la derecha
-    # use a cumulative constraint
+            # Add constraints to link activity to start time and length
+            # A-> b1 y b2
+            # no A -> !b1 y !b2
+            for s, (shift_starts_at, shift_len) in enumerate(zip(t_shifts, len_shifts)):
+                model.Add(shift_starts_at <= t).OnlyEnforceIf(active_shift_at_t[t][s])
+                model.Add(shift_starts_at > t).OnlyEnforceIf(
+                    active_shift_at_t[t][s].Not()
+                )
+                model.Add(t <= shift_starts_at + shift_len - 1).OnlyEnforceIf(
+                    active_shift_at_t[t][s]
+                )
+
+        return active_shift_at_t
--- tests/test_shifts_creation.py
+++ tests/test_shifts_creation.py
@@ -4,19 +4,33 @@ from scripts.shifts.create_shifts import CPShifts
 
 
 @pytest.mark.parametrize(
-    "min_len, max_len, estimated_rider_demand, expected_shifts_by_init_and_len",
+    "min_len, max_len, estimated_rider_demand,"
+    "expected_objective_value, num_expected_shifts_by_init_and_len",
     [
-        (3, 3, {0: 8, 1: 8, 2: 8}, {(0, 3): 8}),
-        (3, 3, {0: 10, 1: 9, 2: 8}, {(0, 3): 9}),  # 9 shifts of len 3 and startime 0
-        (2, 3, {0: 1, 1: 2, 2: 2}, {(0, 3): 1, (1, 2): 1}),
+        (1, 2, {0: 2}, 0, {(0, 1): 2}),
+        # this could return also {(0,2): 2} # make a logic to choose only one option
+        # (1, 2, {0: 2, 1: 2 }, {(0, 1): 2, (1, 1): 2}),
+        (3, 3, {0: 8, 1: 8, 2: 8}, 0, {(0, 3): 8}),
+        # # 9 shifts of len 3 and startime 0 -> obj = 2
+        (3, 3, {0: 10, 1: 9, 2: 8}, 2, {(0, 3): 9}),
+        (2, 3, {0: 1, 1: 2, 2: 2}, 0, {(0, 3): 1, (1, 2): 1}),
+        # (2, 3, {0: 2, 1: 2, 2: 1}, 0, {(0, 3): 1, (1, 2): 1}),  #failing test
     ],
 )
 def test_rider_demand_satisfaction(
-    min_len, max_len, estimated_rider_demand, expected_shifts_by_init_and_len
+    min_len,
+    max_len,
+    estimated_rider_demand,
+    expected_objective_value,
+    num_expected_shifts_by_init_and_len,
 ):
 
     cp_shift = CPShifts(estimated_rider_demand)
-    shifts = cp_shift.create_shifts(min_len=min_len, max_len=max_len)
+    objective, shifts = cp_shift.create_shifts(min_len=min_len, max_len=max_len)
+
+    assert (
+        objective == expected_objective_value
+    ), f"objective value is {objective} and should be {expected_objective_value}"
     assert {
         s: v for s, v in shifts.items() if s[1] > 0
-    } == expected_shifts_by_init_and_len
+    } == num_expected_shifts_by_init_and_len
