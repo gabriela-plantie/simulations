e2bbb0870cf91d0e9f23234b6df07f38595d7c58
gaby <5839522+gabriela-plantie@users.noreply.github.com>
2024-12-13 16:39:22 +0100

    add test cases and symetry criteria

--- tests/tests_optimize_shifts_creation/test_shifts_creation.py
+++ tests/tests_optimize_shifts_creation/test_shifts_creation.py
@@ -57,6 +57,7 @@ model_file = "optimize_shift_creation/create_shifts_mnz.mzn"
         (2, 3, [2, 2, 1], 0, {(0, 2): 1, (0, 3): 1}),
         (2, 3, [2, 2, 1, 2, 2], 0, {(0, 2): 2, (2, 3): 1, (3, 2): 1}),
         (1, 2, [2, 2], 0, {(0, 2): 2}),  # prioritize longer shifts
+        # (1, 2, [10] * 10, 0, {(0, 10): 10}),  # test speed -> dominance
     ],
 )
 def test_staffing_cp_mnz(
